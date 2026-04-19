e93c9a2d2b0e0b7025b2dee2657d3f5ff6f36f31
Oliver Forral <intrepion@gmail.com>
2026-04-19 04:10:28 -0600

    3. Red: Add The Integration Test

--- workspace/integration_test/app_test.dart
+++ workspace/integration_test/app_test.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:prism_face_cropper/adapter/prism_face_cropper_page.dart';
 
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('renders the cropper page title', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: PrismFaceCropperPage()));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Prism Face Cropper'), findsOneWidget);
+  });
+}
